2b9d141db52217482b8451ab288ff708c60f5dcb
markusmj <markus@markusjahn.net>
2019-12-06 13:43:40 +0200

    Organizing Files

---
+++ Delphi Stuff/ECU Tester/ECUCanTest.dpr
@@ -0,0 +1,15 @@
+program ECUCanTest;
+
+uses
+  Vcl.Forms,
+  CanTest in 'CanTest.pas' {MainForm},
+  CanChanEx in 'canchanex.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
