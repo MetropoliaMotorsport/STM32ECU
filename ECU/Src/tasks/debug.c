f6d64a8febc7863c53e2f47be8773a317c75d5e3
Visa Harvey <visa@me.com>
2021-07-06 08:55:06 +0300

    bump version

--- ECU/Src/tasks/debug.c
+++ ECU/Src/tasks/debug.c
@@ -824,7 +824,7 @@ static void DebugTask(void *pvParameters)
 {
 	uint8_t charcount = 0;
 
-	UARTwrite("\r\nBooting ECU b10008...\r\n\r\n");
+	UARTwrite("\r\nBooting ECU b10009...\r\n\r\n");
 
 	redraw = false;
 
