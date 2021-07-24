3ae8cea36b86ae5d7e32188661189caf4fa618cc
Visa Harvey <visa@me.com>
2021-07-24 14:45:10 +0300

    update torque tables for nodes.

--- ECU/Inc/adc_hpf20.h
+++ ECU/Inc/adc_hpf20.h
@@ -9,31 +9,31 @@
 #define ADC_HPF20_H_
 
 
-uint16_t BrakeRInput[sizeof(((eepromdata*)0)->ADCBrakeRPresInput)/2 +3 ] = { 1024, 1025, 	  0, 	 0, 65535 }; // at 240bar, should be 240 output, at 0 bar should be 0 // 62914
+uint16_t BrakeRInput[sizeof(((eepromdata*)0)->ADCBrakeRPresInput)/2 +3 ] = { 0, 0, 	  0, 	 0, 65535 }; // at 240bar, should be 240 output, at 0 bar should be 0 // 62914
 int16_t BrakeROutput[sizeof(((eepromdata*)0)->ADCBrakeRPresOutput)/2 +3 ] = { -1,     0,    0,     240,  255 }; // output range // 240
 uint8_t BrakeRSize = sizeof(BrakeRInput)/sizeof(BrakeRInput[0]);
 
 
-uint16_t BrakeFInput[sizeof(((eepromdata*)0)->ADCBrakeFPresInput)/2 +3 ] = { 1024, 1025,    0,	 0, 65535 }; // at 240bar, should be 240 output, at 0 bar should be 0 // 62914
+uint16_t BrakeFInput[sizeof(((eepromdata*)0)->ADCBrakeFPresInput)/2 +3 ] = { 0, 0,    0,	 0, 65535 }; // at 240bar, should be 240 output, at 0 bar should be 0 // 62914
 int16_t BrakeFOutput[sizeof(((eepromdata*)0)->ADCBrakeFPresOutput)/2 +3 ] = { -1,     0,    0,     240,  255 }; // output range // 240
 uint8_t BrakeFSize = sizeof(BrakeFInput)/sizeof(BrakeFInput[0]);
 
 
 // define zero as 5% actual travel and 100% as 95% of actual travel
-uint16_t TorqueReqLInput[sizeof(((eepromdata*)0)->ADCTorqueReqLInput)/2+4] = {  1024,  1025, 0,     0,     64000,  64001 }; // calibration values for left input // 5800
-int16_t TorqueReqLOutput[sizeof(((eepromdata*)0)->ADCTorqueReqLInput)/2+4] = {  -1,  0,      0,     1000,   1000,  1001 }; // range defined 0-1000 to allow percentage accuracy even if not using full travel range.
+uint16_t TorqueReqLInput[sizeof(((eepromdata*)0)->ADCTorqueReqLInput)/2+4] = {  0,  0, 0,     0,     64000,  64001 }; // calibration values for left input // 5800
+int16_t TorqueReqLOutput[sizeof(((eepromdata*)0)->ADCTorqueReqLInput)/2+4] = {  0,  0,      0,     1000,   1000,  1001 }; // range defined 0-1000 to allow percentage accuracy even if not using full travel range.
 uint8_t TorqueReqLSize = 6; //sizeof(TorqueReqLInput)/sizeof(TorqueReqLInput[0]);
 
 
 // TorqueRMin(6798) / TorqueRMax(54369)
-uint16_t TorqueReqRInput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] =  {  1024,  1025, 0,     0,      64000,  64001 };; // calibration values for right input // 6200
-int16_t TorqueReqROutput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] = { -1,      0,      0,   1000,   1000,   1001 };
+uint16_t TorqueReqRInput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] =  {  0,  0, 0,     0,      64000,  64001 };; // calibration values for right input // 6200
+int16_t TorqueReqROutput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] = { 0,      0,      0,   1000,   1000,   1001 };
 
 uint8_t TorqueReqRSize = 6; //sizeof(TorqueReqRInput)/sizeof(TorqueReqRInput[0]);
 
 // Regen sensor.
-uint16_t BrakeTravelInput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] =  {  1024,  1025, 0,     0,      64000,  64001 };; // calibration values for right input // 6200
-int16_t BrakeTravelOutput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] = { -1,      0,      0,   1000,   1000,   1001 };
+uint16_t BrakeTravelInput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] =  {  0,  0, 0,     0,      64000,  64001 };; // calibration values for right input // 6200
+int16_t BrakeTravelOutput[sizeof(((eepromdata*)0)->ADCTorqueReqRInput)/2+4] = { 0,      0,      0,   1000,   1000,   1001 };
 
 uint8_t BrakeTravelSize = 6; //sizeof(TorqueReqRInput)/sizeof(TorqueReqRInput[0]);
 
