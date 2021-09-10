102b1f89316a1cc7821dd9d2a1d3dd3eed1bd67f
Visa Harvey <visa@me.com>
2021-09-10 15:55:32 +0300

    switch calibration to subtract not add

--- ECU/Src/tasks/adcecu.c
+++ ECU/Src/tasks/adcecu.c
@@ -487,8 +487,8 @@ int getSteeringAnglePWM( void )
 		angle = ( angle*360.0 );
 		angle = angle / 10000; // center around 180;
 		ADCState.SteeringAngleAct = angle;
-		angle += getEEPROMBlock(0)->steerCalib; // account for calibration
-		if ( angle >= 360 ) angle = angle - 360;
+		angle -= getEEPROMBlock(0)->steerCalib; // account for calibration
+		if ( angle < 0 ) angle = angle + 360;
 		angle = (angle - 180);
 		ADCState.SteeringAngle = angle;
 		xTaskNotify( ADCTaskHandle, ( 0x1 << SteeringAngleReceivedBit ), eSetBits);
