f567b60f9659e9a48e47a4e2f963643361dc848f
wecma <78669083+wecma@users.noreply.github.com>
2023-09-17 09:32:57 +0300

    More Config options

--- ECU/Src/tasks/configuration.c
+++ ECU/Src/tasks/configuration.c
@@ -751,15 +751,15 @@ bool DoMenu( uint16_t input )
 
 	static char MenuLines[MAINMENUSIZE+1][21] = { 0 };
 
-	const uint8_t torquevals[] = {0, 5, 10, 15, 18, 20, 25,0}; // zero terminated so function can find end.
+	const uint8_t torquevals[] = {0, 5, 10, 15, 18, 20, 25, 30, 35, 0}; // zero terminated so function can find end.
 
 	const uint8_t torquebalvals[] = {50, 60, 70, 80, 0}; // zero terminated so function can find end.
 
 	const uint8_t fanvals[] = {10,20,30,40,50,60,70,80,90,100,0};
 
-	const uint16_t rpmvals[] = {500, 3000, 5000, 10000, 13000, 15000, 19000, 20000, 0};
+	const uint16_t rpmvals[] = {500, 3000, 5000, 10000, 12500, 13000, 15000, 19000, 20000, 0};
 
-	const uint8_t regenvals[] = {5,10,12,15,20,25,30,0}; // zero terminated so function can find end.
+	const uint8_t regenvals[] = {5,10,12,15,20,25,30, 35, 0}; // zero terminated so function can find end.
 
 	if ( inmenu )
 	{
