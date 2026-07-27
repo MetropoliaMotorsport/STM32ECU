fef7d8f74a4f0d43aca121fb91eaba2636f0f38b
DrinkWaterRightNow <minhnguyen23721@gmail.com>
2026-07-27 18:37:45 +0300

    minor changes

--- ECU/Inc/tasks/canecu.h
+++ ECU/Inc/tasks/canecu.h
@@ -8,43 +8,40 @@
 #ifndef CANECU_H_
 #define CANECU_H_
 
+#include "can_ids.h"
 #include "ecu.h"
 #include "queue.h"
-#include "can_ids.h"
-
 
 // definition of CAN ID's for nodes - Bus definitions not currently used
 
-#define CANBUS2 				hfdcan2
-#define	CANBUS1					hfdcan1
+#define CANBUS2 hfdcan2
+#define CANBUS1 hfdcan1
 
-#define FLSpeed_BUS				CAN1
-#define FRSpeed_BUS				CAN1
+#define FLSpeed_BUS CAN1
+#define FRSpeed_BUS CAN1
 
+#define CANB2 (2)
+#define CANB1 (1)
 
-#define CANB2					(2)
-#define CANB1					(1)
-
-
-#define IVT_BUS					CANB2
-#define BMS_BUS					CANB1
+#define IVT_BUS CANB2
+#define BMS_BUS CANB1
 #define PDM_BUS
 
-#define INV1_BUS				CANB2
-#define INV2_BUS				CANB2
+#define INV1_BUS CANB2
+#define INV2_BUS CANB2
 
-#define Inverter_BUS			CANB2
+#define Inverter_BUS CANB2
 
 ////////////////////////////////////////
-#define NodeErr_ID        		0x600
-#define NodeCmd_ID				0x602
-#define NodeAck_ID				0x601
+#define NodeErr_ID 0x600
+#define NodeCmd_ID 0x602
+#define NodeAck_ID 0x601
 ///////////////////////////////////////
 
 // brake temp sensors 0x1a - first two bytes.
 
-//#define IVT_ID					0x411
-//#define BMS_ID
+// #define IVT_ID					0x411
+// #define BMS_ID
 
 // can open guard/states.
 
@@ -57,84 +54,90 @@
 #define ERROR					0xFF
 */
 
-#define ENABLED					(true)
-#define DISABLED				(false)
+#define ENABLED (true)
+#define DISABLED (false)
 
-enum canbus { bus0, bus1 };
+enum canbus
+{
+  bus0,
+  bus1
+};
 
-typedef struct devices_online{
-	bool BMS;
-	bool IVT;
-	bool Inverter;
-	bool Pedal;
-	bool PWR_Node;
-	bool Node;
+typedef struct devices_online
+{
+  bool BMS;
+  bool IVT;
+  bool Inverter;
+  bool Pedal;
+  bool PWR_Node;
+  bool Node;
 } devices_online;
 
-
-typedef struct can_msg {
-	enum canbus bus;
-	uint16_t id;
-	uint32_t dlc;
-	uint8_t data[8];
+typedef struct can_msg
+{
+  enum canbus bus;
+  uint16_t id;
+  uint32_t dlc;
+  uint8_t data[8];
 } can_msg;
 
 extern QueueHandle_t CanTxQueue;
 
 typedef volatile struct CanDataType CANData;
 
-typedef bool (*DataHandler)(const uint8_t CANRxData[8], const uint32_t DataLength, CANData * datahandle );
-typedef void (*TimeoutHandler)( uint16_t id );
+typedef bool (*DataHandler)(const uint8_t CANRxData[8], const uint32_t DataLength,
+                            CANData* datahandle);
+typedef void (*TimeoutHandler)(uint16_t id);
 
-
-typedef volatile struct CanDataType {
-	volatile DeviceStatus *devicestate;
-	uint16_t id;
-	uint8_t dlcsize;
-	DataHandler getData;
-	TimeoutHandler doTimeout;
-	volatile uint32_t data;
-	uint8_t  index;
-	uint16_t bitpos;
-	uint16_t length;
-	uint32_t timeout;
-	uint32_t time;
-	uint16_t error;
-	uint16_t receiveerr;
-	bool	 errorsent;
+typedef volatile struct CanDataType
+{
+  volatile DeviceStatus* devicestate;
+  uint16_t id;
+  uint8_t dlcsize;
+  DataHandler getData;
+  TimeoutHandler doTimeout;
+  volatile int32_t data;
+  uint8_t index;
+  uint16_t bitpos;
+  uint16_t length;
+  uint32_t timeout;
+  uint32_t time;
+  uint16_t error;
+  uint16_t receiveerr;
+  bool errorsent;
 
 } CANData;
 
-uint8_t CAN1Send( uint16_t id, uint8_t dlc, const uint8_t *pTxData );
-uint8_t CAN2Send( uint16_t id, uint8_t dlc, const uint8_t *pTxData );
-uint8_t CANSendSDO( enum canbus bus, uint16_t id, uint16_t idx, uint8_t sub, uint32_t data);
-char CAN_NMT( uint8_t, uint8_t );
+uint8_t CAN1Send(uint16_t id, uint8_t dlc, const uint8_t* pTxData);
+uint8_t CAN2Send(uint16_t id, uint8_t dlc, const uint8_t* pTxData);
+uint8_t CANSendSDO(enum canbus bus, uint16_t id, uint16_t idx, uint8_t sub, uint32_t data);
+char CAN_NMT(uint8_t, uint8_t);
 
-char CAN_SendErrorStatus( char state, char substate, uint32_t errorcode );
-char CAN_SendStatus( char state, char substate, uint32_t errorcode );
+char CAN_SendErrorStatus(char state, char substate, uint32_t errorcode);
+char CAN_SendStatus(char state, char substate, uint32_t errorcode);
 
-char CAN_NMTSyncRequest( void );
+char CAN_NMTSyncRequest(void);
 
-char CAN_Send4vals( uint16_t id, uint16_t val1, uint16_t val2, uint16_t val3, uint16_t val4 );
+char CAN_Send4vals(uint16_t id, uint16_t val1, uint16_t val2, uint16_t val3, uint16_t val4);
 
-char CAN_SendErrors( void );
+char CAN_SendErrors(void);
 char CAN_SendDebug(uint16_t id);
 
-char reTransmitError(uint32_t canid, const uint8_t *CANRxData, uint32_t DataLength );
-char reTransmitOnCan1(uint32_t canid, const uint8_t *CANRxData, uint32_t DataLength );
+char reTransmitError(uint32_t canid, const uint8_t* CANRxData, uint32_t DataLength);
+char reTransmitOnCan1(uint32_t canid, const uint8_t* CANRxData, uint32_t DataLength);
 
-char CAN_SendTimeBase( void );
+char CAN_SendTimeBase(void);
 
-int CheckCanError( void );
+int CheckCanError(void);
 
-void processCANData(CANData * datahandle, uint8_t * CANRxData, uint32_t DataLength );
-int receivedCANData( CANData * datahandle );
+void processCANData(CANData* datahandle, uint8_t* CANRxData, uint32_t DataLength);
+int receivedCANData(CANData* datahandle);
 
-int RegisterCan1Message(CANData * CanMessage);
-int RegisterCan2Message(CANData * CanMessage);
+int RegisterCan1Message(CANData* CanMessage);
+int RegisterCan2Message(CANData* CanMessage);
 
 // initialisation
 
-int initCAN( void );
+int initCAN(void);
 
 #endif /* CANECU_H_ */
